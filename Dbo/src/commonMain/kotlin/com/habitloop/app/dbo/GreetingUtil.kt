b2b0330d0a81e14adcceffddb654e5bc16761b62
Tepex <tepex@mail.ru>
2026-05-25 21:03:55 +0300

    refactor: simplify src structure

--- Dbo/src/commonMain/kotlin/com/habitloop/app/dbo/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.habitloop.app.dbo
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- Matreshka/src/commonMain/kotlin/com/habitloop/app/matreshka/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.habitloop.app.matreshka
-
-fun sayHello(to: String): String =
-    "Hello, $to! Matreshka"

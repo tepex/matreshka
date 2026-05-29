a2bc6f99529e1aa4108490b474f92db2aa467e27
Tepex <tepex@mail.ru>
2026-05-29 18:08:54 +0300

    fix(habit-new): back button overlapping

--- Habit/src/commonMain/kotlin/ui/NewHabit.kt
+++ Habit/src/commonMain/kotlin/ui/NewHabit.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -118,6 +119,7 @@ fun NewHabitScreen(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .statusBarsPadding()
                     .height(56.dp)
                     .padding(horizontal = 12.dp),
                 contentAlignment = Alignment.CenterStart
@@ -125,6 +127,7 @@ fun NewHabitScreen(
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.clickable { onBackClick() }
+                        .padding(8.dp)
                 ) {
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
